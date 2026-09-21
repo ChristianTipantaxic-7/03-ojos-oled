f75be55eb7969a0600827843e6c95f6526204fc2
TpxCazorla <cristipantaxic@gmail.com>
2026-09-21 14:21:41 +0000

    fix(logo.h): Arregle le logo

--- src/logo.h
+++ src/logo.h
@@ -5,29 +5,14 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
-// Bitmap del logo
-const unsigned char epd_bitmap_descarga[] PROGMEM = {
-  // PEGA AQUÍ TODOS LOS BYTES DEL BITMAP 148x148
-};
-
-// Dimensiones originales del bitmap
-#define LOGO_WIDTH 148
-#define LOGO_HEIGHT 148
-
-// Mostrar el logo centrado, recortando los bordes
 inline void drawLogo(Adafruit_SSD1306 &display) {
   display.clearDisplay();
 
-  // La OLED es 128x64, por eso se muestra
-  // solamente la parte central del logo.
-  display.drawBitmap(
-    -10,
-    -42,
-    epd_bitmap_descarga,
-    LOGO_WIDTH,
-    LOGO_HEIGHT,
-    SSD1306_WHITE
-  );
+  // Estrella
+  display.fillTriangle(64, 2, 76, 42, 110, 42, SSD1306_WHITE);
+  display.fillTriangle(64, 2, 52, 42, 18, 42, SSD1306_WHITE);
+  display.fillTriangle(18, 42, 46, 48, 36, 62, SSD1306_WHITE);
+  display.fillTriangle(110, 42, 82, 48, 92, 62, SSD1306_WHITE);
 
   display.display();
 }
